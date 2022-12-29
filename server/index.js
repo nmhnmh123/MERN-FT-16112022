90fa4c4006116b55f905229d1848739aaa4424de
Frankie Nguyen <frankie.nguyen@nexcel.vn>
2022-12-29 17:03:11 +0700

    feat:  connect to mongo

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://nmhnmh123:minhhai3817@mern-learnit.3kkwpyf.mongodb.net/mern-learnit?retryWrites=true&w=majority`,{
+            //useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            //useFindAndModify: false
+        })
+
+        console.log('Connected to Mongo');
+    } catch (error) {
+        console.log(error.message);
+        process.exit(1)
+    }
+}
+
+connectDB()
 
 const app = express();
 
