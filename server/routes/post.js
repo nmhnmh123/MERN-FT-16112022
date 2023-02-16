7322ac4011507ee4a77d5f4b0f75add162c48599
Frankie Nguyen <frankie.nguyen@nexcel.vn>
2023-02-16 15:50:13 +0700

    start frontend

--- server/routes/post.js
+++ server/routes/post.js
@@ -41,8 +41,71 @@ router.post("/", verifyToken, async (req, res) => {
 */
 router.get("/", verifyToken, async (req, res) => {
   try {
-    const posts = await Post.find({ user: req.userId }).populate('user',['username']);
+    const posts = await Post.find({ user: req.userId }).populate("user", [
+      "username",
+    ]);
     res.json({ success: true, posts });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Error" });
+  }
+});
+
+router.put("/:id", verifyToken, async (req, res) => {
+  const { title, description, url, status } = req.body;
+  if (!title) {
+    return res
+      .status(400)
+      .json({ success: false, message: "title is required" });
+  }
+
+  try {
+    let updatePost = {
+      title,
+      description: description || "",
+      url: (url.startsWith("https://") ? url : `https://${url}`) || "",
+      status: status || "TO LEARN",
+      user: req.userId,
+    };
+
+    const condition = { _id: req.params.id, user: req.userId };
+    updatePost = await Post.findOneAndUpdate(condition, updatePost, {
+      new: true,
+    });
+
+    if (!updatePost) {
+      return res.status(401).json({
+        success: false,
+        message: "Post not found or user not authorized",
+      });
+    }
+
+    res.json({
+      success: true,
+      message: "Update successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Error" });
+  }
+});
+
+router.delete("/:id", verifyToken, async (req, res) => {
+  try {
+    const condition = { _id: req.params.id, user: req.userId };
+    const deletePost = await Post.findOneAndDelete(condition);
+
+    if (!deletePost) {
+      return res.status(401).json({
+        success: false,
+        message: "Post not found or user not authorized",
+      });
+    }
+
+    res.json({
+      success: true,
+      message: "Delete successfully",
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: "Internal Error" });
