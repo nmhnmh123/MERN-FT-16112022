61a8f7b61bca47bbbe828c7496b87d3e216c4346
Frankie Nguyen <frankie.nguyen@nexcel.vn>
2023-02-20 17:07:41 +0700

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Landing}/>
+        <Route exact path='/login' component={Login}/>
+      </Switch>
+    </Router>
   );
 }
 
